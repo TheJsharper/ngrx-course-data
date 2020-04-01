12834f63627addee182e8a498420536b8967bfa1
salomon.pedro <salomon.pedro@fronius.com>
2020-04-01 15:44:06 +0200

    ngrx/data implemented until querying und viewing onto templates

---
+++ src/app/courses/services/course-entity.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { Course } from "../model/course";
+import { EntityCollectionServiceBase, EntityCollectionServiceElementsFactory } from "@ngrx/data";
+
+@Injectable()
+export class CourseEntityService extends EntityCollectionServiceBase<Course>{
+
+    constructor(serviceElementsFactory: EntityCollectionServiceElementsFactory) {
+        super('Course', serviceElementsFactory);
+    }
+}
